084e800894927ebfd907cc2ea1ca56b7282e6f80
Mahmoud Ali <simplymahmoud@gmail.com>
2021-02-15 09:06:27 +0100

    update e2e.js to use page object

--- cypress/integration/e2e.js
+++ cypress/integration/e2e.js
@@ -1,83 +1,73 @@
 /// <reference types="cypress" />
-import {
-    navigate,
-    generateRandom,
-    addTodo,
-    editTodo,
-    validateTodoText,
-    toggleTodo,
-    clearCompleted,
-    validateTodoCompletedState,
-    validateToggleState,
-    validateNumberOfTodosShown,
-  } from '../actions/e2e-actions'
-  
+
+  import {TodoPage} from '../actions/e2e-actions'
 
   describe('todo actions', () => {
+    const todoPage = new TodoPage()
     beforeEach(() => {
-      navigate()
+        todoPage.navigate()
     })
   
     describe('positive tests', () => {
         it('should add a new todo to the list', () => {
-            const todo_task = generateRandom()
-            addTodo(todo_task)
-            validateTodoText(0, todo_task)
-            validateToggleState(0, false)
+            const todo_task = todoPage.generateRandom()
+            todoPage.addTodo(todo_task)
+            todoPage.validateTodoText(0, todo_task)
+            todoPage.validateToggleState(0, false)
         })
         
         it('should edit todo on the list', () => {
-            const todo_task = generateRandom()
-            addTodo(todo_task)
-            editTodo(0, todo_task+todo_task)
-            validateTodoText(0, todo_task+todo_task)
-            validateToggleState(0, false)
+            const todo_task = todoPage.generateRandom()
+            todoPage.addTodo(todo_task)
+            todoPage.editTodo(0, todo_task+todo_task)
+            todoPage.validateTodoText(0, todo_task+todo_task)
+            todoPage.validateToggleState(0, false)
         })
 
         it('should toggle test correctly', () => {
-            const todo_task = generateRandom()
-            addTodo(todo_task)
-            toggleTodo(0)
-            validateTodoCompletedState(0, true)
-            clearCompleted()
+            const todo_task = todoPage.generateRandom()
+            todoPage.addTodo(todo_task)
+            todoPage.toggleTodo(0)
+            todoPage.validateTodoCompletedState(0, true)
+            todoPage.clearCompleted()
         })
   
         it('should clear completed', () => {
-            const todo_task = generateRandom()
-            addTodo(todo_task)
-            addTodo(todo_task)
-            toggleTodo(0)
-            toggleTodo(1)
-            clearCompleted()
-            validateNumberOfTodosShown(0)
+            const todo_task = todoPage.generateRandom()
+            todoPage.addTodo(todo_task)
+            todoPage.addTodo(todo_task)
+            todoPage.toggleTodo(0)
+            todoPage.toggleTodo(1)
+            todoPage.clearCompleted()
+            todoPage.validateNumberOfTodosShown(0)
         })
     })
 
     describe('negative tests', () => {
         it('should add a new todo to the list; long text', () => {
-            const special_task = generateRandom()+generateRandom()+generateRandom()+generateRandom()+generateRandom()+generateRandom()
-            addTodo(special_task)
-            validateTodoText(0, special_task)
-            validateToggleState(0, false)
+            const special_task = todoPage.generateRandom()+todoPage.generateRandom()+todoPage.generateRandom()+todoPage.generateRandom()+todoPage.generateRandom()+todoPage.generateRandom()
+            todoPage.addTodo(special_task)
+            todoPage.validateTodoText(0, special_task)
+            todoPage.validateToggleState(0, false)
         })
     
         it('should add a new todo to the list; special chars text', () => {
             const special_task = '+_=-)(*&^#!~`[];\',.<>\/'
-            addTodo(special_task)
-            validateTodoText(0, special_task)
-            validateToggleState(0, false)
+            todoPage.addTodo(special_task)
+            todoPage.validateTodoText(0, special_task)
+            todoPage.validateToggleState(0, false)
         })
             
         it('should not add a new todo to the list; empty text', () => {
             const special_task = ''
-            addTodo(special_task)
-            validateNumberOfTodosShown(0)
+            todoPage.addTodo(special_task)
+            todoPage.validateNumberOfTodosShown(0)
         })
          
         it('should not add a new todo to the list; spaces text', () => {
             const special_task = '                  '
-            addTodo(special_task)
-            validateNumberOfTodosShown(0)
+            todoPage.addTodo(special_task)
+            todoPage.validateNumberOfTodosShown(0)
         })
       })
 
